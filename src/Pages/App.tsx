ea912e9e7a9232ce795d0f7f514e1a7ff296fef0
Josh Daniels <82977701+CodingComp@users.noreply.github.com>
2025-02-01 08:31:53 -0700

    Github Pages Working
    Website now viewable through github pages. Changes config to solve blank page issue.
    
    Also updated packages to latest.

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -4,12 +4,12 @@ import FeaturedProject from "./FeaturedProject.tsx";
 function App() {
   return (
     <>
-      <h1>Josh Daniel's Portfolio</h1>
+      <h1>Josh Daniels Portfolio</h1>
       <FeaturedProject
         projectName="Portfolio Website"
-        projectDescription="This is my portfolio website created using React and TypeScript. This website is self hosted using cloudflare. This website is used as a means to demonstrate my programming skills and to showcase my projects."
+        projectDescription="This is my portfolio website created using React and TypeScript. This site is hosted through github pages."
         projectImage="https://www.pbs.org/wgbh/nova/media/images/JWST_hero.width-2000.jpg"
-        projectLink="https://github.com/CodingComp/portfolio-website"
+        projectLink="https://github.com/CodingComp/portfolio"
       />
     </>
   );
