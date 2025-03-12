6ffe6f7e7703eaf1e3dfc0c697bf50b3b2575f72
Josh Daniels <82977701+CodingComp@users.noreply.github.com>
2025-03-12 16:21:36 -0600

    Route Debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import {Route, HashRouter} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HomePage from "./Pages/HomePage.tsx";
@@ -7,9 +7,11 @@ import "./CSS/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename='/portfolio'>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/AnagramSolver" element={<AnagramProject />} />
-    </HashRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/AnagramSolver" element={<AnagramProject />}></Route>
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
